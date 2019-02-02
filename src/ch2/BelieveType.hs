8e8f617c76ed34a4dbc43445218fee12f7bfcc58
jinhye <wlsgp012@naver.com>
2019-02-02 10:04:17 +0900

    ch2

---
+++ src/ch2/BelieveType.hs
@@ -0,0 +1,8 @@
+module BelieveType where
+
+removeNonUppercase :: String -> String
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
