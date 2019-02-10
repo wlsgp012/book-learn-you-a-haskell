c3c128e0b4c36c72075ce39101dfe2b0ccc2e1de
jinhye <jinhye.kim@navercorp.com>
2019-02-10 10:36:23 +0900

    ch7 Shape

---
+++ src/ch7/Shape.hs
@@ -0,0 +1,28 @@
+module Shape( Point(..)
+            , Shape(..)
+            , surface
+            , nudge
+            , baseCircle
+            , baseRect)
+where
+
+data Point = Point Float Float
+data Shape = Circle Point Float | Rectangle Point Point deriving Show
+-- data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving Show
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+circleX x = Circle x 4 5
+result = map circleX [1, 2, 3]
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
