023d8358e4bd4df42a5399553f44d562ac0afdd8
jinhye <jinhye.kim@navercorp.com>
2019-02-14 00:08:25 +0900

    ch7 Recursive

---
+++ src/ch7/Recursive.hs
@@ -0,0 +1,34 @@
+module Recursive where
+
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+-- data List a = Empty | Cons {listHead :: a, listTail:: List a} deriving (Show, Read, Eq, Ord)
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+ | x == a = Node x left right
+ | x < a = Node a (treeInsert x left) right
+ | x > a = Node a left (treeInsert x right)
+
+treeInsert2 a b = flip treeInsert a b
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+ | x == a = True
+ | x < a = treeElem x left
+ | x > a = treeElem x right
+
