1807e135600017372dbee226dfb93dfad3a40d95
jinhye <wlsgp012@naver.com>
2019-03-05 23:55:14 +0900

    ch14 writer

---
+++ src/ch14/ApplyLog.hs
@@ -0,0 +1,20 @@
+module ApplyLog where
+
+import Data.Monoid
+
+isBigGang :: Int -> (Bool, String)
+isBigGang x = (x>9, "Compared gang size to 9.")
+
+-- applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+-- applyLog :: (a, [c]) -> (a -> (b, [c])) -> (b, [c])
+applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+-- applyLog (x, log) f = let (y, newLog) = f x in (y, log ++ newLog)
+applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "jerky" = ("whiskey", Sum 99)
+addDrink _ = ("beer", Sum 30)
---
+++ src/ch14/Gcd.hs
@@ -0,0 +1,45 @@
+module Gcd where
+
+import           Control.Monad.Writer
+import           Data.Monoid
+import           Data.Semigroup
+-- gcd' :: Int -> Int -> Int
+-- gcd' a b
+--  | b == 0  = a
+--  | otherwise = gcd' b (a `mod` b)
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+ | b == 0  = do
+  tell ["Finished with " ++ show a]
+  return a
+ | otherwise = do
+  tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+  gcd' b (a `mod` b)
+
+gcdReverse :: Int -> Int -> Writer [String] Int
+gcdReverse a b
+ | b == 0  = do
+  tell ["Finished with " ++ show a]
+  return a
+ | otherwise = do
+  result <- gcdReverse b (a `mod` b)
+  tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+  return result
+
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+-- instance Semigroup (DiffList a) where
+--  (<>) = (++)
+--  DiffList x <> DiffList y = DiffList (\x y -> x ++ y)
+
+instance Monoid (DiffList a) where
+    mempty = DiffList (\xs -> [] ++ xs)
+    (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
