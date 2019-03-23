2c163f30d18c6869fa66fc7261cd803537896b0a
jinhye <wlsgp012@naver.com>
2019-03-23 11:34:30 +0900

    Add tarai function

---
+++ src/Tarai.hs
@@ -0,0 +1,19 @@
+module Tarai where
+
+tarai :: Int -> Int -> Int -> Int
+tarai x y z =
+ if x <= y then y
+ else tarai
+           (tarai (x - 1) y z)
+           (tarai (y - 1) z x)
+           (tarai (z - 1) x y)
+
+
+tarai' :: Int -> Int -> Int -> Int
+tarai' x y z
+ | x <= y = y
+ | otherwise = tarai
+                    (tarai (x - 1) y z)
+                    (tarai (y - 1) z x)
+                    (tarai (z - 1) x y)
+
