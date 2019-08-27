728d87b2e7429c5a2d026b686abf5d46870ea715
jinhye <wlsgp012@naver.com>
2019-08-27 20:04:24 +0900

    Add fun to ch2

--- src/ch2/BelieveType.hs
+++ src/ch2/BelieveType.hs
@@ -1,8 +1,16 @@
 module BelieveType where
 
 removeNonUppercase :: String -> String
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A' .. 'Z'] ]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
+factorial :: Integer -> Integer
+factorial n = product [1 .. n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
